00f4adf3efe2f887f615c1f446ecc6dd29fe519c
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-10-08 22:20:14 +0300

    Entity Framework

---
+++ Exercises - ADO.NET/2. Villain Names/Config.cs
@@ -0,0 +1,8 @@
+﻿namespace _2._Villain_Names
+{
+    internal static class Config
+    {
+        public const string ConnectionString =
+            @"Server=DESKTOP-TMJTP0L\SQLEXPRESS;Database=MinionsDB;Integrated Security=true;TrustServerCertificate=true;";
+    }
+}
