d6d1f12327f657fd71274c1cbb941dc4cf178ac5
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-11-04 13:42:37 +0200

    Car DealerShip Exercise

---
+++ JavaScript Object Notation - JSON/Car DealerShip/CarDealer/StartUp.cs
@@ -0,0 +1,15 @@
+﻿namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+
+        }
+
+        public static string ImportUsers(ProductShopContext context, string inputJson)
+        {
+            
+        }
+    }
+}
