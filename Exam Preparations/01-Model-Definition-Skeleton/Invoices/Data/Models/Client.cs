0f6aecc562f97b13bc8a6f45822334b481a93ac7
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-11-18 19:56:40 +0200

    Added database
    Exam database added to the sql server

---
+++ Exam Preparations/01-Model-Definition-Skeleton/Invoices/Data/Models/Client.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Invoices.Data.Models
+{
+    public class Client
+    {
+        public Client()
+        {
+            Addresses = new HashSet<Address>();
+            ProductsClients = new HashSet<ProductClient>();
+            Invoices = new HashSet<Invoice>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(25)]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        [StringLength(15)]
+        public string NumberVat { get; set; } = null!;
+
+        public virtual ICollection<Invoice> Invoices { get; set; }
+
+        public virtual ICollection<Address> Addresses { get; set; }
+
+        public virtual ICollection<ProductClient> ProductsClients { get; set; }
+    }
+}
---
+++ Exam Preparations/01-Model-Definition-Skeleton/Invoices/Data/Models/Product.cs
@@ -0,0 +1,28 @@
+﻿using Invoices.Data.Models.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace Invoices.Data.Models
+{
+    public class Product
+    {
+        public Product()
+        {
+            ProductsClients = new HashSet<ProductClient>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        public string Name { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+
+        [Required]
+        public CategoryType CategoryType { get; set; }
+
+        public virtual ICollection<ProductClient> ProductsClients { get; set; }
+    }
+}
