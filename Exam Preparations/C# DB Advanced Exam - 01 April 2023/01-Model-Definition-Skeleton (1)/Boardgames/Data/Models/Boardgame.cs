18d011e4b5c931a0369a9b24c14dbac6eb4c53e7
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-11-21 14:44:56 +0200

    Boardgames database

---
+++ Exam Preparations/C# DB Advanced Exam - 01 April 2023/01-Model-Definition-Skeleton (1)/Boardgames/Data/Models/Boardgame.cs
@@ -0,0 +1,42 @@
+﻿using Boardgames.Data.Models.Enums;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Boardgames.Data.Models
+{
+    public class Boardgame
+    {
+        public Boardgame()
+        {
+            BoardgamesSellers = new HashSet<BoardgameSeller>();    
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        public double Rating { get; set; }
+
+        [Required]
+        public int YearPublished { get; set; }
+
+        [Required]
+        public CategoryType CategoryType { get; set; }
+
+        [Required]
+        public string Mechanics { get; set; } = null!;
+
+        [Required]
+        public int CreatorId { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(CreatorId))]
+        public virtual Creator Creator { get; set; } = null!;
+
+        public virtual ICollection<BoardgameSeller> BoardgamesSellers { get; set; }
+    }
+}
---
+++ Exam Preparations/C# DB Advanced Exam - 01 April 2023/01-Model-Definition-Skeleton (1)/Boardgames/DataProcessor/Deserializer.cs
@@ -0,0 +1,87 @@
+﻿namespace Boardgames.DataProcessor
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.Text;
+    using Boardgames.Data;
+    using Boardgames.Data.Models;
+    using Boardgames.Data.Models.Enums;
+    using Boardgames.DataProcessor.ImportDto;
+    using Boardgames.Utilities;
+    using Microsoft.EntityFrameworkCore;
+
+    public class Deserializer
+    {
+        private const string ErrorMessage = "Invalid data!";
+
+        private const string SuccessfullyImportedCreator
+            = "Successfully imported creator – {0} {1} with {2} boardgames.";
+
+        private const string SuccessfullyImportedSeller
+            = "Successfully imported seller - {0} with {1} boardgames.";
+
+        public static string ImportCreators(BoardgamesContext context, string xmlString)
+        {
+            XmlHelper helper = new();
+
+            ImportCreatorsDto[] creatorsDtos = helper.Deserialize<ImportCreatorsDto[]>(xmlString, "Creators");
+
+            StringBuilder sb = new();
+            ICollection<Creator> validCreators = new HashSet<Creator>();
+
+            foreach (var creatorDto in creatorsDtos)
+            {
+                if (!IsValid(creatorDto))
+                {
+                    sb.AppendLine(ErrorMessage);
+                    continue;
+                }
+
+                Creator creator = new Creator()
+                {
+                    FirstName = creatorDto.FirstName,
+                    LastName = creatorDto.LastName,
+                };
+
+                foreach (var boardgameDto in creatorDto.BoardGames)
+                {
+                    if (!IsValid(boardgameDto))
+                    {
+                        sb.AppendLine(ErrorMessage);
+                        continue;
+                    }
+
+                    Boardgame boardgame = new Boardgame()
+                    {
+                        Name = boardgameDto.Name,
+                        Rating = boardgameDto.Rating,
+                        YearPublished = boardgameDto.YearPublished,
+                        CategoryType = boardgameDto.CategoryType,
+                        Mechanics = boardgameDto.Mechanics
+                    };
+
+                    creator.Boardgames.Add(boardgame);
+                }
+
+                validCreators.Add(creator);
+                sb.AppendLine(String.Format(SuccessfullyImportedCreator, creator.FirstName, creator.LastName, creator.Boardgames.Count));
+            }
+
+            context.Creators.AddRange(validCreators);
+            context.SaveChanges();
+            return sb.ToString().TrimEnd();
+        }
+
+        public static string ImportSellers(BoardgamesContext context, string jsonString)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static bool IsValid(object dto)
+        {
+            var validationContext = new ValidationContext(dto);
+            var validationResult = new List<ValidationResult>();
+
+            return Validator.TryValidateObject(dto, validationContext, validationResult, true);
+        }
+    }
+}
