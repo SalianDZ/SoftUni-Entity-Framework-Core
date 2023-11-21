9240397c105561a0eada1a4c8eec7a6e3aa9c0a2
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-11-22 00:56:11 +0200

    Export and Import

--- Exam Preparations/C# DB Advanced Exam - 01 April 2023/01-Model-Definition-Skeleton (1)/Boardgames/DataProcessor/Serializer.cs
+++ Exam Preparations/C# DB Advanced Exam - 01 April 2023/01-Model-Definition-Skeleton (1)/Boardgames/DataProcessor/Serializer.cs
@@ -1,17 +1,68 @@
 ﻿namespace Boardgames.DataProcessor
 {
     using Boardgames.Data;
+    using Boardgames.Data.Models;
+    using Boardgames.DataProcessor.ExportDto;
+    using Boardgames.Utilities;
+    using Newtonsoft.Json;
+    using System.Collections.Immutable;
+    using System.Text.Json.Nodes;
 
     public class Serializer
     {
         public static string ExportCreatorsWithTheirBoardgames(BoardgamesContext context)
         {
-            throw new NotImplementedException();
+            XmlHelper helper = new();
+
+            ExportCreatorDto[] creators = context.Creators
+                .Where(c => c.Boardgames.Count >= 1)
+                .OrderByDescending(c => c.Boardgames.Count).ThenBy(c => c.FirstName)
+                .Select(c => new ExportCreatorDto
+                {
+                    BoardgamesCount = c.Boardgames.Count,
+                    CreatorName = c.FirstName + " " + c.LastName,
+                    Boardgames = c.Boardgames.Select(bc => new ExportBoardgameInCreatordto
+                    { 
+                        Name = bc.Name,
+                        YearPublished = bc.YearPublished,
+                    })
+                    .OrderBy(bc => bc.Name)
+                    .ToArray()
+                }).ToArray();
+
+            return helper.Serialize<ExportCreatorDto[]>(creators, "Creators");
         }
 
         public static string ExportSellersWithMostBoardgames(BoardgamesContext context, int year, double rating)
         {
-            throw new NotImplementedException();
+            var sellers = context
+                .Sellers
+                .Where(s => s.BoardgamesSellers.Any(bs => bs.Boardgame.YearPublished >= year && bs.Boardgame.Rating <= rating))
+                .ToArray()
+                .Select(s => new
+                {
+                    s.Name,
+                    s.Website,
+                    Boardgames = s.BoardgamesSellers
+                        .Where(bs => bs.Boardgame.YearPublished >= year && bs.Boardgame.Rating <= rating)
+                        .ToArray()
+                        .OrderByDescending(bs => bs.Boardgame.Rating)
+                        .ThenBy(bs => bs.Boardgame.Name)
+                        .Select(bs => new
+                        {
+                            Name = bs.Boardgame.Name,
+                            Rating = bs.Boardgame.Rating,
+                            Mechanics = bs.Boardgame.Mechanics,
+                            Category = bs.Boardgame.CategoryType.ToString()
+                        })
+                        .ToArray()
+                })
+                .OrderByDescending(s => s.Boardgames.Length)
+                .ThenBy(s => s.Name)
+                .Take(5)
+                .ToArray();
+
+            return JsonConvert.SerializeObject(sellers, Formatting.Indented);
         }
     }
 }
