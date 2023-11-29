f08d7f8efc2e02e806f8cf414c47f5b9ebd2843b
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-11-29 15:18:40 +0200

    Database

---
+++ Exam Preparations/C# DB Advanced Retake Exam - 16 Dec 2021/01. Model Definition_Skeleton (2)/Artillery/Data/Models/Manufacturer.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Artillery.Data.Models
+{
+    public class Manufacturer
+    {
+        public Manufacturer()
+        {
+            Guns = new HashSet<Gun>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(40)]
+        public string ManufacturerName { get; set; } = null!;
+
+        [Required]
+        [StringLength(100)]
+        public string Founded { get; set; } = null!;
+
+        public virtual ICollection<Gun> Guns { get; set; }
+    }
+}
