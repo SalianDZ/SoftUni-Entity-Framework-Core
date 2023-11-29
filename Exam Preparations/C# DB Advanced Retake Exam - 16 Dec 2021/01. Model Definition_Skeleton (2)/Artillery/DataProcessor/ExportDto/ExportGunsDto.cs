6eb5e9e30bf83903b06b0410ae549cf2836ae07c
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-11-30 01:24:52 +0200

    Data Export

---
+++ Exam Preparations/C# DB Advanced Retake Exam - 16 Dec 2021/01. Model Definition_Skeleton (2)/Artillery/DataProcessor/ExportDto/ExportGunsDto.cs
@@ -0,0 +1,26 @@
+﻿using System.Xml.Serialization;
+
+namespace Artillery.DataProcessor.ExportDto
+{
+    [XmlType("Gun")]
+    public class ExportGunsDto
+    {
+        [XmlAttribute]
+        public string Manufacturer { get; set; } = null!;
+
+        [XmlAttribute]
+        public string GunType { get; set; } = null!;
+
+        [XmlAttribute]
+        public int GunWeight { get; set; }
+
+        [XmlAttribute]
+        public double BarrelLength { get; set; }
+
+        [XmlAttribute]
+        public int Range { get; set; }
+
+        [XmlArray]
+        public ExportCountryDto[] Countries { get; set; } = null!;
+    }
+}
--- Exam Preparations/C# DB Advanced Retake Exam - 16 Dec 2021/01. Model Definition_Skeleton (2)/Artillery/DataProcessor/Serializer.cs
+++ Exam Preparations/C# DB Advanced Retake Exam - 16 Dec 2021/01. Model Definition_Skeleton (2)/Artillery/DataProcessor/Serializer.cs
@@ -2,17 +2,87 @@
 namespace Artillery.DataProcessor
 {
     using Artillery.Data;
+    using Artillery.DataProcessor.ExportDto;
+    using Newtonsoft.Json;
+    using System.Text;
+    using System.Text.Json.Nodes;
+    using System.Xml.Serialization;
 
     public class Serializer
     {
         public static string ExportShells(ArtilleryContext context, double shellWeight)
         {
-            throw new NotImplementedException();
+
+            var shells = context.Shells
+                .Where(s => s.ShellWeight > shellWeight)
+                .OrderBy(s => s.ShellWeight)
+                .ToArray()
+                .Select(s => new
+                {
+                    s.ShellWeight,
+                    s.Caliber,
+                    Guns = s.Guns
+                    .Where(g => g.GunType.ToString() == "AntiAircraftGun")
+                    .ToArray()
+                    .Select(s => new
+                    {
+                        GunType = s.GunType.ToString(),
+                        s.GunWeight,
+                        s.BarrelLength,
+                        Range = GetRangeMethod(s.Range)
+                    })
+                    .OrderByDescending(g => g.GunWeight)
+                    .ToArray()
+                })
+                .ToArray();
+
+            return JsonConvert.SerializeObject(shells, Formatting.Indented);
         }
 
         public static string ExportGuns(ArtilleryContext context, string manufacturer)
         {
-            throw new NotImplementedException();
+            var guns = context.Guns
+                .Where(g => g.Manufacturer.ManufacturerName == manufacturer)
+                .OrderBy(g => g.BarrelLength)
+                .ToArray()
+                .Select(g => new ExportGunsDto()
+                {
+                    Manufacturer = g.Manufacturer.ManufacturerName,
+                    GunType = g.GunType.ToString(),
+                    GunWeight = g.GunWeight,
+                    BarrelLength = g.BarrelLength,
+                    Range = g.Range,
+                    Countries = g.CountriesGuns.Where(c => c.Country.ArmySize > 4500000)
+                    .ToArray()
+                        .Select(cg => new ExportCountryDto()
+                        {
+                            Country = cg.Country.CountryName,
+                            ArmySize = cg.Country.ArmySize
+                        })
+                        .OrderBy(cg => cg.ArmySize)
+                        .ToArray()
+                })
+                .ToArray();
+
+            StringBuilder sb = new StringBuilder();
+
+            XmlRootAttribute xmlRoot =
+                new XmlRootAttribute("Guns");
+            XmlSerializer xmlSerializer =
+                new XmlSerializer(typeof(ExportGunsDto[]), xmlRoot);
+
+            XmlSerializerNamespaces namespaces = new XmlSerializerNamespaces();
+            namespaces.Add(string.Empty, string.Empty);
+
+            using StringWriter writer = new StringWriter(sb);
+            xmlSerializer.Serialize(writer, guns, namespaces);
+
+            return sb.ToString().TrimEnd();
+        }
+
+        private static string GetRangeMethod(int range)
+        {
+            return range > 3000 ? "Long-range" : "Regular range";
         }
     }
 }
